146f0d38b60fafbba58ad4f656dfb212cffb3f66
ilias-hhu <aoskenshin@gmail.com>
2021-11-02 04:41:09 +0100

    teamspeak command

---
+++ src/commands/Utility/teamspeak.js
@@ -0,0 +1,51 @@
+"use strict";
+
+const { SlashCommandBuilder } = require("@discordjs/builders");
+const { colorPalette } = require("../../util/util.js");
+const { CommandInteraction, Permissions, MessageEmbed, IntegrationApplication } = require("discord.js");
+const Guild = require("../../models/guilds.js");
+
+module.exports.cooldown = {
+    length: 5000, /* in ms */
+    users: new Set()
+};
+
+/**
+ * Runs teamspeak command.
+ * @param {CommandInteraction} interaction The Command Interaciton
+ * @param {any} utils Additional util
+ */
+module.exports.run = async (interaction, utils) =>
+{
+    await interaction.deferReply({ ephemeral: false });
+
+    const embed = new MessageEmbed()
+        .setAuthor("Zero-V Roleplay - TeamSpeak", interaction.client.user.avatarURL({ format: "png" }))
+        .setColor(colorPalette.brandColor);
+
+    const currentGuild = await Guild.findOne({ id: interaction.guildId });
+    if (!currentGuild)
+    {
+        embed.setDescription(":x: | Dieser Server muss zuerst mit `/setup` eingerichtet werden.");
+        await interaction.editReply({ embeds: [embed] });
+        return;
+    }
+    if (!currentGuild.teamspeak)
+    {
+        embed.setDescription(":x: | Für diesen Server wurde noch kein TeamSpeak Server hinterlegt.");
+        await interaction.editReply({ embeds: [embed] });
+        return;
+    }
+
+    embed.setDescription(`**TeamSpeak IP:** ${currentGuild.teamspeak}`);
+    await interaction.editReply({ embeds: [embed] });
+};
+
+module.exports.permissions = {
+    clientPermissions: Permissions.FLAGS.SEND_MESSAGES,
+    userPermissions: Permissions.FLAGS.SEND_MESSAGES
+};
+
+module.exports.data = new SlashCommandBuilder()
+    .setName("teamspeak")
+    .setDescription("Ruft die IP für den TeamSpeak Server ab.");
