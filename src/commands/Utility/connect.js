8ecdba9930411c49cfcab2a6fab7d8317fad2cec
ilias-hhu <aoskenshin@gmail.com>
2021-11-02 04:41:28 +0100

    connect command

---
+++ src/commands/Utility/connect.js
@@ -0,0 +1,51 @@
+"use strict";
+
+const { SlashCommandBuilder } = require("@discordjs/builders");
+const { colorPalette } = require("../../util/util.js");
+const { CommandInteraction, Permissions, MessageEmbed, IntegrationApplication } = require("discord.js");
+const Guild = require("../../models/guilds.js");
+
+module.exports.cooldown = {
+    length: 5000, /* in ms */
+    users: new Set()
+};
+
+/**
+ * Runs connect command.
+ * @param {CommandInteraction} interaction The Command Interaciton
+ * @param {any} utils Additional util
+ */
+module.exports.run = async (interaction, utils) =>
+{
+    await interaction.deferReply({ ephemeral: false });
+
+    const embed = new MessageEmbed()
+        .setAuthor("Zero-V Roleplay - Connect ", interaction.client.user.avatarURL({ format: "png" }))
+        .setColor(colorPalette.brandColor);
+
+    const currentGuild = await Guild.findOne({ id: interaction.guildId });
+    if (!currentGuild)
+    {
+        embed.setDescription(":x: | Dieser Server muss zuerst mit `/setup` eingerichtet werden.");
+        await interaction.editReply({ embeds: [embed] });
+        return;
+    }
+    if (!currentGuild.connect)
+    {
+        embed.setDescription(":x: | Für diesen Server wurde noch kein Connect Link hinterlegt.");
+        await interaction.editReply({ embeds: [embed] });
+        return;
+    }
+
+    embed.setDescription(`**FiveM Connect Link:** ${currentGuild.connect}`);
+    await interaction.editReply({ embeds: [embed] });
+};
+
+module.exports.permissions = {
+    clientPermissions: Permissions.FLAGS.SEND_MESSAGES,
+    userPermissions: Permissions.FLAGS.SEND_MESSAGES
+};
+
+module.exports.data = new SlashCommandBuilder()
+    .setName("connect")
+    .setDescription("Ruft den FiveM Connect Link ab.");
