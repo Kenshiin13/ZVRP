d228ac78ed597c4136a3d0f8b3bf3f869c282c2d
ilias-hhu <aoskenshin@gmail.com>
2021-11-14 12:34:03 +0100

    try/catch in tick

--- src/util/tick.js
+++ src/util/tick.js
@@ -8,27 +8,34 @@ const { actions } = require("../util/util.js");
  */
 module.exports = async function (client)
 {
-    setInterval(async () =>
+    try
     {
-        const now = new Date();
-        const res = await TempAction.find({
-            length: {
-                "$lte": now
-            }
-        }).populate(["guildId", "action"]);
-        if (res.length != 0)
+        setInterval(async () =>
         {
-            res.forEach(async (action) =>
-            {
-                if (action.type == actions.mute)
-                {
-                    const member = await client.guilds.cache.get(action.guildId.id).members.fetch(action.target);
-                    if (action.guildId.mutedrole)
-                        await member.roles.remove(action.guildId.mutedrole);
+            const now = new Date();
+            const res = await TempAction.find({
+                length: {
+                    "$lte": now
                 }
+            }).populate(["guildId", "action"]);
+            if (res.length != 0)
+            {
+                res.forEach(async (action) =>
+                {
+                    if (action.type == actions.mute)
+                    {
+                        const member = await client.guilds.cache.get(action.guildId.id).members.fetch(action.target);
+                        if (action.guildId.mutedrole)
+                            await member.roles.remove(action.guildId.mutedrole);
+                    }
 
-                await action.remove();
-            });
-        }
-    }, 60000);
+                    await action.remove();
+                });
+            }
+        }, 60000);
+    }
+    catch (err)
+    {
+
+    }
 };
